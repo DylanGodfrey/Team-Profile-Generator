ffa2805ce32c28b3a82cdb4cc53853cf61c739db
DylanGodfrey <39961099+DylanGodfrey@users.noreply.github.com>
2022-11-07 02:16:42 -0500

    Reduced inquirer functions down to one

--- index.js
+++ index.js
@@ -7,123 +7,94 @@ const Manager = require("./lib/Manager");
 const inquirer = require("inquirer");
 
 // TODO: Repeating similiar questions isn't very DRY, replace with `${role}`s?
-const questions = [
-  "Manager's Name:",
-  "Manager's ID:",
-  "Manager's Email:",
-  "Manager's Office Number:",
-  "Engineer's Name:",
-  "Engineer's ID:",
-  "Engineer's Email:",
-  "Engineer's Github:",
-  "Intern's Name:",
-  "Intern's ID:",
-  "Intern's Email:",
-  "Intern's school:",
-  "Add another Employee?",
-];
+
 
 // Array to select what kind of Employee to add, or end the queries
 const chooseRole = ["Engineer", "Intern", "Finish"];
 
 init();
-// TODO: Each of these fucntions are very similar, make new methods to reduce redundancies?
-function init() {
+async function init() {
   // ADD a new Manager Employee
-  inquirer
-    .prompt([
-      { type: "input", message: questions[0], name: "name" },
-      { type: "input", message: questions[1], name: "id" },
-      { type: "input", message: questions[2], name: "email" },
-      { type: "input", message: questions[3], name: "office" },
-      // Query if they would like to add another employee
-      {
-        type: "list",
-        message: questions[12],
-        choices: chooseRole,
-        name: "add",
-      },
-    ])
-    .then((response) => {
-
-      // create the new Manager using queried info
-      const manager = new Manager(
-        response.name,
-        response.id,
-        response.email,
-        response.office
-      );
-      // Go through query process again using that role's specific queries for each new Employee until told to stop
-      if (response.add === "Engineer") {
-        addEngineer();
-      } else if (response.add === "Intern") {
-        addIntern();
-      } else {
-        console.log("end");
-      }
-    });
+  await addEmployee("Manager");
 }
 
 // ADD a new Engineer Employee
-function addEngineer() {
-  inquirer
-    .prompt([
-      { type: "input", message: questions[4], name: "name" },
-      { type: "input", message: questions[5], name: "id" },
-      { type: "input", message: questions[6], name: "email" },
-      { type: "input", message: questions[7], name: "github" },
-      {
-        type: "list",
-        message: questions[12],
-        choices: chooseRole,
-        name: "add",
-      },
-    ])
-    .then((response) => {
-      const engineer = new Engineer(
-        response.name,
-        response.id,
-        response.email,
-        response.github
-      );
-      if (response.add === "Engineer") {
-        addEngineer();
-      } else if (response.add === "Intern") {
-        addIntern();
-      } else {
-        console.log("end");
-      }
-    });
-}
+async function addEmployee(selectedRole) {
 
-// ADD a new Intern Employee
-function addIntern() {
-  inquirer
+  const genericQuestions = [
+    `${selectedRole}'s Name:`,
+    `${selectedRole}'s ID:`,
+    `${selectedRole}'s Email:`,
+    "Add another Employee?",
+  ]
+  
+
+  const roleSpecific = [
+    `${selectedRole}'s Office Number:`,
+    `${selectedRole}'s Github:`,
+    `${selectedRole}'s School:`,
+  ];
+
+  switch (selectedRole) {
+    case "Manager":
+      roleIndex = 0;
+      break;
+    case "Engineer":
+      roleIndex = 1;
+      break;
+    case "Intern":
+      roleIndex = 2;
+      break;
+  }
+  
+
+  // Manager prompts
+  await inquirer
     .prompt([
-      { type: "input", message: questions[8], name: "name" },
-      { type: "input", message: questions[9], name: "id" },
-      { type: "input", message: questions[10], name: "email" },
-      { type: "input", message: questions[11], name: "school" },
+      { type: "input", message: genericQuestions[0], name: "name" },
+      { type: "input", message: genericQuestions[1], name: "id" },
+      { type: "input", message: genericQuestions[2], name: "email" },
+      { type: "input", message: roleSpecific[roleIndex], name: "specific" },
       {
         type: "list",
-        message: questions[12],
+        message: genericQuestions[3],
         choices: chooseRole,
         name: "add",
       },
     ])
     .then((response) => {
-      const intern = new Intern(
-        response.name,
-        response.id,
-        response.email,
-        response.school
-      );
+
+      // Can't do a single variable like: const employee = new `${selectedRole}` for all of them? So have to create each Class specifically
+      switch (selectedRole) {
+        case "Engineer":
+          const engineer = new Engineer(
+            response.name,
+            response.id,
+            response.email,
+            response.specific
+          );
+        case "Intern":
+          const intern = new Intern(
+            response.name,
+            response.id,
+            response.email,
+            response.specific
+          );
+          case "Manager":
+          const manager = new Manager(
+            response.name,
+            response.id,
+            response.email,
+            response.specific
+          );
+      }
       if (response.add === "Engineer") {
-        addEngineer();
+        addEmployee("Engineer");
       } else if (response.add === "Intern") {
-        addIntern();
+        addEmployee("Intern");
       } else {
         console.log("end");
       }
-    });
+    },
+  );
 }
